bf10dd2724c76f39c95b40124f519ea2c369b8e6
Airblow <143172177+Airblow@users.noreply.github.com>
2023-11-24 10:36:20 +0100

    Prog.Olympiad

---
+++ OlympiadUppgift1/OlympiadUppgift1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace OlympiadUppgift6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Vad vill du skriva och hur många gånger ska det upprepas? Skriv t.ex 'hej 6' för att skriva hej 6 gånger. Endast mellan 1 och 9.");
+                string[] userInput = Console.ReadLine().Split(" ");
+
+                int repeating = int.Parse(userInput[1]);
+
+                Console.WriteLine();
+
+                if (repeating < 10 && repeating > 0)
+                    for (int i = 0; i < repeating; i++)
+                    {
+                        Console.Write(userInput[0]);                   
+                    }
+                else
+                {
+                    Console.WriteLine("Du angav ett ogiltligt tal");
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("Något blev fel. Kom ihåg, endast tal 1-9.");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OlympiadUppgift2/OlympiadUppgift2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace olympiadUppgift2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Skriv in ett heltal som är större än 0.");
+                long userNumber = long.Parse(Console.ReadLine());
+                long product = 0;
+                int firstVariable = 1;
+
+                while (userNumber > product)
+                {
+                    product = firstVariable * (firstVariable + 1) * (firstVariable + 2);
+                    firstVariable++;
+
+                }
+
+                Console.WriteLine($"Det finns {firstVariable - 2} treiga tal som är mindre än {userNumber}.");
+                Console.ReadKey();
+
+            }
+            catch
+            {
+                Console.WriteLine("Något gick fel. Följde du instruktionerna?");
+            }
+
+        }
+
+    }
+
+}
